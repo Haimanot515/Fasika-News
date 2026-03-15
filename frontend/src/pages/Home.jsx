7f1f2c7e6c580b054907617b65727435ccd34531
Haimanot Beka <haimanotbeka@gmail.com>
2026-03-15 09:45:08 +0300

    home and landing file added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Landing />
+      <Home />
     </div>
   );
 }
